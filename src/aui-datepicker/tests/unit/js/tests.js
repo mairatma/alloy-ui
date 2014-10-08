224c8897b90ea32e6b80b0198afa592f420e254d
michael.bradford@liferay.com <michael.bradford@liferay.com>
2014-10-07 18:01:55 -0700

    AUI-1205 - add unit test for multiple triggers

--- src/aui-datepicker/tests/unit/js/tests.js
+++ src/aui-datepicker/tests/unit/js/tests.js
@@ -4,44 +4,82 @@ YUI.add('aui-datepicker-tests', function(Y) {
 
     suite.add(new Y.Test.Case({
         name: 'Datepicker Tests',
+
+        selectDate: function(datePicker, dayIndex) {
+            var popover = datePicker.getPopover(),
+                dayCells = popover.bodyNode.all('.yui3-calendar-day'),
+                toClick = dayCells.item(dayIndex);
+
+            toClick.simulate('click');
+        },
+
         'selectionChange event should only fire when selection changes': function() {
             var selectionChangeCount = 0,
-                trigger = Y.one('#trigger');
+                trigger = Y.one('#trigger'),
 
-            var datePicker = new Y.DatePicker({
-                on: {
-                    selectionChange: function(event) {
-                        selectionChangeCount++;
-                    }
-                },
-                popover: {
-                    zIndex: 1
-                },
-                panes: 1,
-                trigger: '#trigger'
-            });
-
-            var popover = datePicker.getPopover();
+                datePicker = new Y.DatePicker({
+                    on: {
+                        selectionChange: function(event) {
+                            selectionChangeCount++;
+                        }
+                    },
+                    popover: {
+                        zIndex: 1
+                    },
+                    panes: 1,
+                    trigger: '#trigger'
+                });
 
             Y.Assert.areEqual(0, selectionChangeCount);
-
-            datePicker.show();
 
             trigger.simulate('click');
 
             Y.Assert.areEqual(0, selectionChangeCount);
 
-            var dayCell = popover.bodyNode.one('.yui3-calendar-day');
-
-            dayCell.simulate('click');
+            this.selectDate(datePicker, 0);
 
             Y.Assert.areEqual(1, selectionChangeCount);
 
             trigger.simulate('click');
 
-            Y.Assert.areEqual(1, selectionChangeCount);
+            this.selectDate(datePicker, 1);
 
-            dayCell.next().simulate('click');
+            Y.Assert.areEqual(2, selectionChangeCount);
+        },
+
+        'selectionChange event should not fire when switching between datepickers': function() {
+            var selectionChangeCount = 0,
+                triggerA = Y.one('#triggerA'),
+                triggerB = Y.one('#triggerB'),
+
+                datePicker = new Y.DatePicker({
+                    on: {
+                        selectionChange: function(event) {
+                            selectionChangeCount++;
+                        }
+                    },
+                    popover: {
+                        zIndex: 1
+                    },
+                    panes: 1,
+                    trigger: '#triggerA, #triggerB'
+                });
+
+            triggerA.simulate('click');
+
+            this.selectDate(datePicker, 0);
+
+            triggerB.simulate('click');
+
+            this.selectDate(datePicker, 1);
+
+            Y.Assert.areEqual(2, selectionChangeCount);
+
+            triggerA.simulate('click');
+
+            Y.Assert.areEqual(2, selectionChangeCount);
+
+            triggerB.simulate('click');
 
             Y.Assert.areEqual(2, selectionChangeCount);
         }
